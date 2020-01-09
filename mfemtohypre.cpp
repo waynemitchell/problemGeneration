1d2aa95e94b92a61eec51bbd248f65a88736b0cc
Wayne Mitchell <wmitchell@mp-headnode.ziti.uni-heidelberg.de>
2020-01-09 10:41:44 +0100

    Fixed issue for no global partition in hypre.

--- mfemtohypre.cpp
+++ mfemtohypre.cpp
@@ -8,12 +8,12 @@ void MFEMtoHYPRE(HypreParMatrix &A, Vector &B, Vector &X,  HYPRE_ParCSRMatrix *A
 {
    *A_out = A.StealData();
 
-   HYPRE_Int *partitioning;
    HYPRE_Int global_m, global_n;
-   HYPRE_ParCSRMatrixGetRowPartitioning(*A_out, &partitioning);
    HYPRE_ParCSRMatrixGetDims(*A_out, &global_m, &global_n);
-   HYPRE_ParVectorCreate(hypre_MPI_COMM_WORLD, global_m, partitioning, B_out);
-   HYPRE_ParVectorCreate(hypre_MPI_COMM_WORLD, global_m, partitioning, X_out);
+   HYPRE_ParVectorCreate(hypre_MPI_COMM_WORLD, global_n, hypre_ParCSRMatrixRowStarts(*A_out), B_out);
+   HYPRE_ParVectorCreate(hypre_MPI_COMM_WORLD, global_m, hypre_ParCSRMatrixColStarts(*A_out), X_out);
+   hypre_ParVectorSetPartitioningOwner(*B_out, 0);
+   hypre_ParVectorSetPartitioningOwner(*X_out, 0);
    hypre_VectorData(hypre_ParVectorLocalVector(*B_out)) = B.StealData();
    hypre_VectorData(hypre_ParVectorLocalVector(*X_out)) = X.StealData();
    HYPRE_ParVectorInitialize(*B_out);
